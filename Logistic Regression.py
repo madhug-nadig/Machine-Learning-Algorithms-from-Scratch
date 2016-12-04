ad5561e58b44176857f2cc45bbaf5574c6622847
Madhu G Nadig <madhug.nadig@gmail.com>
2016-12-04 13:48:54 +0530

    Continue

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -1,6 +1,6 @@
 #================================================================================================================
 #----------------------------------------------------------------------------------------------------------------
-#													LOGISTIC REGRESSION
+#									LOGISTIC REGRESSION
 #----------------------------------------------------------------------------------------------------------------
 #================================================================================================================
 
@@ -27,36 +27,40 @@ class CustomLogisticRegression:
 		self.alpha = 0.1
 		self.lambd = 0.25
 		self.iter = 2500
+		self.x = x
+		self.y = y
 		
 		#initialie theta
 		self.theta = np.random.rand(x.shape[1],1)
 	
 	#the cost function
-	def cost_fn(self, m, x, y):
-		pass
+	def cost_fn(self, m):
+		h = self.sigmoid_function(np.dot(self.x, self.theta)) #get the hypothesis
+		J = ( 1.0/m ) * np.sum( -self.y * (np.log (h)) - ( 1.0 -self.y ) * (np.log(1.0 -h)) ) #This is the cost that needs to be paid by the learning algorithm if the outcome is hθ(x) and the actual outcome is y
+		return J
 
 	#The sigmoid function
 	def sigmoid_function(z):
 		return 1.0 / ( 1.0 + math.e**(-1*z) ) #Using 1.0 to make it a floating point type
 	
 	#Gradient function
-	def gradients(self, m, x, y):
+	def gradients(self, m):
 		zrd = self.theta
 		zrd[0, :] = 0
-		h = sigmoid_function(np.dot(x, self.theta))
-		return ( 1.0/m ) * np.dot(x.T, ( h - y ) ) + (float(self.lambd)/m) * zrd
+		h = self.sigmoid_function(np.dot(self.x, self.theta)) #get the hypothesis
+		return ( 1.0/m ) * np.dot(self.x.T, ( h - self.y ) ) + (float(self.lambd)/m) * zrd 
 
 	#This is batch
-	def descent(self, x, y):
+	def descent(self):
 		for i in range(self.iter):
-			self.cost.append( cost_fn(x.shape[0], x, y))
-			gradientz = gradients(x.shape[0], x , y)
+			self.cost.append( self.cost_fn(self.x.shape[0]))
+			gradientz = self.gradients(x.shape[0])
 			
 			#Change theta based on the "gradientz"
 			self.theta[0, :] = gradientz[0, :] - self.alpha * gradientz[0, :]
 			self.theta[1, :] = gradientz[1:, :] - self.alpha * gradientz[1:, :]
 		
-		pred = np.dot(x, self.theta)
+		pred = np.dot(self.x, self.theta)
 		pred[ pred >= 0.5 ] = 1
 		pred[ pred < 0.5 ] = 0
 
@@ -84,7 +88,7 @@ def main():
 	x_test, y_test, x_train, y_train = train_test_split(x,y, test_size = 0.1, random_state = 0)
 	
 	glm = CustomLogisticRegression(x, y)
-	
+	glm.descent()
 	plt.scatter(x[:,0], y)
 	
 	plt.show()
