64ac3e1de3d9140bc7aa9aad44bc3d91c3eb824a
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-27 23:52:08 +0530

    Start fit method SVM

--- Support Vector Machine.py
+++ Support Vector Machine.py
@@ -18,6 +18,27 @@ class CustomSVM:
 	
 	def __init__(self):
 		pass
+		
+		
+	#Use the data and find a 'model' ie the values for W and b. Maximize b and minimize b 
+	def fit(self, dataset):
+		self.dataset = dataset
+		
+		# Magnitude of W is the key, list of W and b is the value
+		options = {}
+		
+		trans =  [[1,1],[-1,1],[-1,-1],[1 ,-1]]
+		data = []
+		
+		for yi, attrs in self.dataset.items():
+			for attr in attrs:
+				for f in attr:
+					print(f, attr)
+					data.append(f)
+		print(data)
+		self.max_attr = max(data)
+		self.min_attr = min(data)
+	
 
 def main():
 	svm = CustomSVM()
