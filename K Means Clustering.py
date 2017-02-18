7c4ef1b71a8e6e17c38af524676bb178b70f2f07
Madhu G Nadig <madhug.nadig@gmail.com>
2017-02-18 21:06:12 +0530

    Add fit

--- K Means Clustering.py
+++ K Means Clustering.py
@@ -5,13 +5,51 @@ class K_Means:
 		self.k = k
 		self.tolerance = tolerance
 		self.max_iterations = max_iterations
-	
+
 	def fit(self, data):
-		pass
+		self.centroids = {}
+
+		#initialize the centroids, the first 'k' elements in the dataset will be our initial centroids
+		for i in range(self.k):
+			self.centroids[i] = data[i]
+		
+		#begin iterations
+		for i in range(self.max_iterations):
+			self.classes = {}
+			for i in range(self.k):
+				self.classes[i] = []
+			
+			#find the distance between the point and cluster; choose the nearest centroid
+			for features in data:
+				distances = [np.linalg.norm(features - self.centroids[centroid]) for centroid in self.centroids]
+				classification = distances.index(min(distances))
+				self.classes[classification].append(features)
+
+			previous = dict(self.centroids)
+
+			#average the cluster datapoints to re-calculate the centroids
+			for classification in self.classes:
+				self.centroids[classification] = np.average(self.classes[classification], axis = 0)
+
+			isOptimal = True
+
+			for centroid in self.centroids:
+
+				original_centroid = previous[centroid]
+				curr = self.centroids[centroid]
+
+				if np.sum((curr - original_centroid)/original_centroid * 100.0) > self.tolerance:
+					isOptimal = False
+
+			#break out of the main loop if the results are optimal, ie. the centroids don't change their positions much(more than our tolerance)
+			if isOptimal:
+				break
 
 	def pred(self, data):
 		pass
 
+
 X = np.array([[1,2],[1,1],[1.5,1.4],[4,4],[4,6],[5,5],[6,8], [3,1],[2,2],[9,9],[5,5],[1,0.5],[7,7]])
 
-km = K_Means(2)
+km = K_Means(3)
+km.fit(X)
