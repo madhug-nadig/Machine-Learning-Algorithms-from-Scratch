33d20f02148a51395ffca9d671e12e78743e3f14
Nadig <madhug.nadig@gmail.com>
2020-07-04 13:26:23 +0200

    Update SVM

--- Support Vector Machine.py
+++ Support Vector Machine.py
@@ -15,44 +15,42 @@ import datetime
 plt.style.use('ggplot')
 
 class CustomSVM:
-	
+
 	def __init__(self):
 		pass
-		
-		
-	#Use the data and find a 'model' ie the values for W and b. Maximize b and minimize b 
+
+
+	#Use the data and find a 'model' ie the values for W and b. Maximize b and minimize b
 	def fit(self, dataset):
 		self.dataset = dataset
-		
+
 		# Magnitude of W is the key, list of W and b is the value
 		options = {}
-		
-		trans =  [[1,1],[-1,1],[-1,-1],[1 ,-1]]
-		data = []
-		
-		for yi, attrs in self.dataset.items():
-			for attr in attrs:
-				for f in attr:
-					print(f, attr)
-					data.append(f)
-		print(data)
-		self.max_attr = max(data)
-		self.min_attr = min(data)
-		
-		#Clear the data from memory
-		data = None
-		
-		step_size = [self.max_attr * 0.1,self.max_attr * 0.01,self.max_attr * 0.005]
-		
-		b_range = 5
+
+
+		all_feature_values = []
+
+	    for yi, attrs in self.dataset.items():
+	      for attr in attrs:
+	        for f in attr:
+	          all_feature_values.append(f)
+
+	    self.max_attr = max(data)
+	    self.min_attr = min(data)
+	    del all_feature_values
+
+    	step_size = [self.max_attr * 0.1,self.max_attr * 0.01,self.max_attr * 0.005]
+    	latest_optimum = float(inf)
+
+		b_range = 3
 		b_multiple = 5
-		
-		latest_optimum = self.max_attr * 10
-		
+
+		trans =  [[1,1],[-1,1],[-1,-1],[1 ,-1]]
+
 		for step in step_size:
 			W = np.array([latest_optimum,latest_optimum])
-			opti = False
-			
+			optimization_flag = False
+
 			while not opti:
 				for b in np.arange(-1*(self.max_attr* b_range ), self.max_attr * b_range, step * b_multiple):
 					for transformation in trans:
@@ -66,23 +64,23 @@ class CustomSVM:
 						if found:
 							options[np.linalg.norm(W_t)] = [W_t, b]
 				if W[0]<0:
-					opti = True
+					optimization_flag = True
 					print("Optimized by a step: ", step)
 				else:
 					W -= step
-			
+
 			norms = min([n for n in options])
 			self.W = options[norms][0]
 			self.b = options[norms][1]
-			
+
 			latest_optimum = options[norms][0][0] + step*2
 
 	def predict(self, attrs):
 		#sign of the X(i).W + b defines the class
 		classification = np.sign(np.dot(np.array(attrs), self.W) + self.b)
-		
+
 		return classification
-			
+
 def main():
 
 	dataset = { -1 : np.array([[2,3],[4,5],[2,1]]), 1: np.array([[5,6], [8,8], [9,9]]) }
@@ -90,6 +88,6 @@ def main():
 
 	svm.fit(dataset = dataset)
 	pred = svm.predict(attrs = [2,2])
-	
+
 if __name__ == "__main__":
 	main()
