f4c46c1146f92cee49ed69be4eb7ac13a31b6d80
Nadig <madhug.nadig@gmail.com>
2020-07-11 21:19:12 +0200

    Fix SVM issues

--- Support Vector Machine.py
+++ Support Vector Machine.py
@@ -30,17 +30,17 @@ class CustomSVM:
 
 		all_feature_values = []
 
-	    for yi, attrs in self.dataset.items():
-	      for attr in attrs:
-	        for f in attr:
-	          all_feature_values.append(f)
+		for yi, attrs in self.dataset.items():
+			for attr in attrs:
+				for f in attr:
+					all_feature_values.append(f)
 
-	    self.max_attr = max(data)
-	    self.min_attr = min(data)
-	    del all_feature_values
+		self.max_attr = max(all_feature_values)
+		self.min_attr = min(all_feature_values)
+		del all_feature_values
 
-    	step_size = [self.max_attr * 0.1,self.max_attr * 0.01,self.max_attr * 0.005]
-    	latest_optimum = float(inf)
+		step_size = [self.max_attr * 0.1,self.max_attr * 0.01,self.max_attr * 0.005]
+		latest_optimum = 10 * self.max_attr
 
 		b_range = 3
 		b_multiple = 5
@@ -51,14 +51,14 @@ class CustomSVM:
 			W = np.array([latest_optimum,latest_optimum])
 			optimization_flag = False
 
-			while not opti:
+			while not optimization_flag:
 				for b in np.arange(-1*(self.max_attr* b_range ), self.max_attr * b_range, step * b_multiple):
 					for transformation in trans:
 						W_t = W * transformation
 						found = True
 						for yi, xi in self.dataset.items():
-							print(b)
-							if not (yi* np.dot(W_t , xi)+b ) >= 1:
+
+							if not all((yi * np.dot(xi, W_t)  +b )) >= 1:
 								found = False
 								break
 						if found:
@@ -67,7 +67,8 @@ class CustomSVM:
 					optimization_flag = True
 					print("Optimized by a step: ", step)
 				else:
-					W -= step
+					print(W)
+					W = np.array(list(map(lambda w: w-step, W)))
 
 			norms = min([n for n in options])
 			self.W = options[norms][0]
